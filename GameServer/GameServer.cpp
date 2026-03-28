15e3e6dea3d83e1760449c8736edd33329439a0a
Dodontak <110716928+Dodontak@users.noreply.github.com>
2026-03-28 13:48:31 +0900

    Feat: NetAddress
    - NetAddress
    생성자, 복사생성자 추가
    getter 추가.

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,4 +3,7 @@
 int main()
 {
 	SocketUtils::Init();
+
+	NetAddress addr("127.0.0.1", 7777);
+	cout << addr.GetIp() << ":" << addr.GetPort() << endl;
 }
--- ServerCore/NetAddress.cpp
+++ ServerCore/NetAddress.cpp
@@ -1,11 +1,26 @@
 #include "pch.h"
 #include "NetAddress.h"
+#include <ws2tcpip.h>
 
-NetAddress::NetAddress()
+NetAddress::NetAddress(string ip, int port)
 {
+	::memset(&_sockAddr, 0, sizeof(_sockAddr));
+	_sockAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, ip.c_str(), &_sockAddr.sin_addr);
+	_sockAddr.sin_port = ::htons(port);
+}
 
+NetAddress::NetAddress(const NetAddress& addr)
+{
+	_sockAddr = addr._sockAddr;
 }
 
 NetAddress::~NetAddress()
 {
+}
+
+string NetAddress::GetIp()
+{
+	char buff[INET_ADDRSTRLEN];
+	return ::inet_ntop(AF_INET, &_sockAddr.sin_addr, buff, sizeof(buff));
 }
