e2a41594da844bc1804d95ce11d400166b5139fb
Dodontak <110716928+Dodontak@users.noreply.github.com>
2026-04-26 14:31:06 +0900

    Feat: 클라이언트를 위한 protoc 21.12추가
    protoc 21.12 exe파일 추가
    
    GenPacket.bat 파일 수정
    - 클라이언트용 pb.h, cc파일은 21.12버전의 protoc로 생성해서 복사해주도록 수정함. 상대경로를 이용함에 유의
    
    exe파일을 git에 포함하는게 좀 이상한데 일단 이렇게 함.

--- Protocol/GenPacket.bat
+++ Protocol/GenPacket.bat
@@ -9,24 +9,31 @@ GenPackets.exe --path=./Protocol.proto --output=ServerPacketHandler --recv=S_ --
 
 IF ERRORLEVEL 1 PAUSE
 
-XCOPY /Y Enum.pb.h "..\GameServer\"
-XCOPY /Y Enum.pb.cc "..\GameServer\"
-XCOPY /Y Struct.pb.h "..\GameServer\"
-XCOPY /Y Struct.pb.cc "..\GameServer\"
-XCOPY /Y Protocol.pb.h "..\GameServer\"
-XCOPY /Y Protocol.pb.cc "..\GameServer\"
+XCOPY /Y *.pb.h "..\GameServer\"
+XCOPY /Y *.pb.cc "..\GameServer\"
 XCOPY /Y ClientPacketHandler.h "..\GameServer\"
 
-XCOPY /Y Enum.pb.h "..\DummyClient\"
-XCOPY /Y Enum.pb.cc "..\DummyClient\"
-XCOPY /Y Struct.pb.h "..\DummyClient\"
-XCOPY /Y Struct.pb.cc "..\DummyClient\"
-XCOPY /Y Protocol.pb.h "..\DummyClient\"
-XCOPY /Y Protocol.pb.cc "..\DummyClient\"
+XCOPY /Y *.pb.h "..\DummyClient\"
+XCOPY /Y *.pb.cc "..\DummyClient\"
 XCOPY /Y ServerPacketHandler.h "..\DummyClient\"
+
+IF ERRORLEVEL 1 PAUSE
+
+DEL /Q /F *.pb.h
+DEL /Q /F *.pb.cc
+
+protoc_v21_12 -I=./ --cpp_out=./ ./Enum.proto
+protoc_v21_12 -I=./ --cpp_out=./ ./Struct.proto
+protoc_v21_12 -I=./ --cpp_out=./ ./Protocol.proto
+
+XCOPY /Y *.pb.h "..\..\Client\Source\Client\Network\"
+XCOPY /Y *.pb.cc "..\..\Client\Source\Client\Network\"
+XCOPY /Y ServerPacketHandler.h "..\..\Client\Source\Client\"
 
 DEL /Q /F *.pb.h
 DEL /Q /F *.pb.cc
 DEL /Q /F *.h
+
+IF ERRORLEVEL 1 PAUSE
 
 PAUSE
